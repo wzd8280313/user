10ffa901ad26c2eddaaa3bf6c314213dbc6e3996
weipinglee <weiping.lee@163.com>
2015-07-24 14:59:42 +0800

    2015-7-24

---
+++ config/config.php
@@ -0,0 +1,62 @@
+<?php return array (
+  'logs' => 
+  array (
+    'path' => 'backup/logs/log',
+    'type' => 'file',
+  ),
+  'DB' => 
+  array (
+    'type' => 'mysqli',
+    'tablePre' => 'shop_',
+    'read' => 
+    array (
+      0 => 
+      array (
+        'host' => 'localhost:3306',
+        'user' => 'root',
+        'passwd' => '127890',
+        'name' => 'iweb',
+      ),
+    ),
+    'write' => 
+    array (
+      'host' => 'localhost:3306',
+      'user' => 'root',
+      'passwd' => '127890',
+      'name' => 'iweb',
+    ),
+  ),
+  'interceptor' => 
+  array (
+    0 => 'themeroute@onCreateController',
+    1 => 'layoutroute@onCreateView',
+    2 => 'hookCreateAction@onCreateAction',
+    3 => 'hookFinishAction@onFinishAction',
+  ),
+  'langPath' => 'language',
+  'viewPath' => 'views',
+  'skinPath' => 'skin',
+  'classes' => 'classes.*',
+  'rewriteRule' => 'pathinfo',
+  'theme' => 
+  array (
+    'pc' => 'index',
+    'mobile' => 'default',
+  ),
+  'skin' => 
+  array (
+    'pc' => 'black',
+    'mobile' => 'black',
+  ),
+  'timezone' => 'Etc/GMT-8',
+  'upload' => 'upload',
+  'dbbackup' => 'backup/database',
+  'safe' => 'session',
+  'lang' => 'zh_sc',
+  'debug' => false,
+  'configExt' => 
+  array (
+    'site_config' => 'config/site_config.php',
+  ),
+  'encryptKey' => 'b01cb56e21fe2810a987aadcd74828dc',
+)?>
