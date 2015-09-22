62f8100d3b8c1a7dc442a95411689db9d343aa02
weipinglee <weiping.lee@163.com>
2015-09-22 16:43:58 +0800

    2015-9-22 首页广告闪

--- views/scsg/javascript/a.js
+++ views/scsg/javascript/a.js
@@ -1,23 +1,24 @@
 
 $(document).ready(function(){
-	$(".floor .fl-cnt .fl-rcmd .fl-rcmd-bg .admanage a").mouseenter(function(){
+		$('.admanage').each(function(){
+			var img = $(this).find('img');
+			var w = img.css('width');
+			var h = img.css('height');
+			var shine = $('.shine').eq(0).clone(true);
+			shine.css('width',w).css('height',h);
+			$(this).append(shine);
+		});
+	$(".floor  .admanage").on('mouseenter',function(){
 		$(this).find(".shine").stop();
 		$(this).find(".shine").css("opacity","0.4"); 
 		$(this).find(".shine").animate({opacity: '0'},500);
-	});
-});
-$(document).ready(function(){
-	$(".floor .fl-cnt .fl-rcmd1 .fl-rcmd-bg1 .admanage a").mouseenter(function(){
-		$(this).find(".shine").stop();
-		$(this).find(".shine").css("opacity","0.4"); 
-		$(this).find(".shine").animate({opacity: '0'},500);
-	});
-});
-
-$(document).ready(function(){
+	})
+	
+	
 	$(".goods-list .goods-img").mouseenter(function(){
 		$(this).find(".shine").stop();
 		$(this).find(".shine").css("opacity","0.4"); 
 		$(this).find(".shine").animate({opacity: '0'},500);
 	});
 });
+
