509f557b748c6da153890f2e80a2ccdef4b99af0
weipinglee <weiping.lee@163.com>
2015-10-18 21:34:40 +0800

    2015-10-18 个人中心

--- views/mobile/javascript/common.js
+++ views/mobile/javascript/common.js
@@ -386,4 +386,47 @@ function getKeywords(url,_this){
 	})
 }
 
+function ajax_get_data(obj){
+	
+	var url = obj.url ? obj.url : false;
+	var callback = obj.callback ? obj.callback : null;
+	var template_id = obj.template_id ? obj.template_id : false;
+	var append_to = obj.append_to ? obj.append_to : false;
+	var append_after = obj.append_after ? obj.append_after : 1;
+	if(!obj.trans_data.page)obj.trans_data.page = parseInt($('input[name=page]').val());
+	if(!url || !template_id || !append_to)
+		return false;
+	
+	$('.loading-imgS img').show();
+	$('.loading-imgS p').hide();
 
+	
+	$.ajax({
+		type:'post',
+		async:false,
+		data:obj.trans_data,
+		dataType:'json',
+		url:url,
+		beforeSend:function(){
+			
+		},
+		success:function(data){
+			if(data==0){
+				$('.loading-imgS p').text('没有更多数据');
+			}else{
+				for(var i in data){
+					var newPro = template.render(template_id,data[i]);
+					append_to.append(newPro);
+				}
+				$('input[name=page]').val(obj.page+1);
+			}
+		},
+		complete:function(){
+			$('.loading-imgS img').hide();
+			$('.loading-imgS p').show();
+		},
+		timeout:1000,
+	})
+	
+	
+}
