12fe8c86e8518cc7fbdff25b7ebbf7bea6bfd30f
zyn1 <739619279@qq.com>
2016-01-29 10:13:08 +0800

    删除调试代码

--- classes/refunds_class.php
+++ classes/refunds_class.php
@@ -11,8 +11,7 @@ Class Refunds_Class{
 	 * @brief 判断商品是否可退货
 	 * @$orderGoodsRow array 包含order_goods 和order表数据
 	 */
-	public static function order_goods_refunds($orderGoodsRow){
-        var_dump($orderGoodsRow);exit;
+	public static function order_goods_refunds($orderGoodsRow){     
 		if($orderGoodsRow['order_amount']==0)return false;
 		if($orderGoodsRow['refunds_status']==0 && in_array($orderGoodsRow['status'],array(2,7,8,9)))
 			return true;
