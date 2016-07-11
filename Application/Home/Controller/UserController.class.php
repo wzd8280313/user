1282b8b7c4abe8c19a67dc2f2b7e5e81599111b9
wangzhande <15313086535@163.com>
2016-07-11 18:04:15 +0800

    练习

---
+++ Application/Home/Controller/UserController.class.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * user: Administrator
+ * Date: 2016/7/11
+ * Time: 17:03
+ */
+
+namespace Home\Controller;
+use Think\Controller;
+
+class UserController extends Controller{
+
+    public function loginAction(){
+        $this->display();
+    }
+    public function registerAction(){
+
+        $this->display();
+    }
+}
